efc5d6d0ae5ae5f9b83aae7b2b95c488ee9b69ad
Helweg <kenneth@helweghansen.dk>
2017-04-25 11:46:35 +0200

    Working - missing policies

--- csvToDict.py
+++ csvToDict.py
@@ -3,10 +3,10 @@ output = open('setCommands','w')
 with open('IJOS-Alternative-Virtual-SRX-A1.csv', 'r') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		if row['Interface'] != '' and row['ip address'] != '' and row['Peer Interface'] == '' and row['Peer Interface'] == 'N/A':
+		if (row['Interface'] != '' and row['ip address'] != '') and (row['Peer Interface'] == '' or row['Peer Interface'] == 'N/A'):
 			output.write('set interface ' + row['Interface'] + ' unit ' + row['Unit'] + ' family ' + row['Family'] + ' address ' + row['ip address']+row['Mask'] +'\n')
 		if row['Interface'] != '' and row['ip address'] != '' and row['Peer Interface'] != '' and row['Peer Interface'] != 'N/A':
-			output.write('set interface ' + row['Interface'] + ' unit ' + row['Unit'] + ' encapsulation ' + row['Encapsulation'] + ' peer-unit' + row['Peer Unit'] + ' family ' + row['Family'] + ' address ' + row['ip address']+row['Mask'] +'\n')
+			output.write('set interface ' + row['Interface'] + ' unit ' + row['Unit'] + ' encapsulation ' + row['Encapsulation'] + ' peer-unit ' + row['Peer Unit'] + ' family ' + row['Family'] + ' address ' + row['ip address']+row['Mask'] +'\n')
 		if row['Routing-Instance'] != '' and row['Main Router Virtual Router'] != '':
 			output.write('set routing-instances ' + row['Main Router Virtual Router'] + ' instance-type ' + row['Routing-Instance'] +'\n')
 		if row['Routing-Instance'] == 'virtual-router':
