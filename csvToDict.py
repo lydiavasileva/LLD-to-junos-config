77e240bdcb4fb495cb981177610499743a85fcc6
Helweg <kenneth@helweghansen.dk>
2017-04-25 09:55:28 +0200

    added setup of protocols

--- csvToDict.py
+++ csvToDict.py
@@ -1,17 +1,24 @@
 import csv
+output = open('setCommands','w')
 with open('IJOS-Alternative-Virtual-SRX-A1.csv', 'r') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
 		if row['Interface'] != '' and row['ip address'] != '':
-			print('set interface ' + row['Interface'] + ' unit ' + row['Unit'] + ' family ' + row['Family'] + ' address ' + row['ip address']+row['Mask'])
+			output.write('set interface ' + row['Interface'] + ' unit ' + row['Unit'] + ' family ' + row['Family'] + ' address ' + row['ip address']+row['Mask'] +'\n')
 		if row['Routing-Instance'] != '' and row['Main Router Virtual Router'] != '':
-			print('set routing-instances ' + row['Main Router Virtual Router'] + ' instance-type ' + row['Routing-Instance'])
+			output.write('set routing-instances ' + row['Main Router Virtual Router'] + ' instance-type ' + row['Routing-Instance'] +'\n')
 		if row['Routing-Instance'] == ' virtual-router':
 			if row['Main Router Virtual Router'] != '':
 				deviceName = row['Main Router Virtual Router']
-				print('set routing-instances ' + deviceName + ' interface ' + row['Interface'] + '.' + row['Unit'])
+				output.write('set routing-instances ' + deviceName + ' interface ' + row['Interface'] + '.' + row['Unit'] +'\n')
 			else:
-				print('set routing-instances ' + deviceName + ' interface ' + row['Interface'] + '.' + row['Unit'])
-		
-		#print(row['Main Router Virtual Router'],row['Instance-Type'], row['Interface'], row['Unit'], row['ip address'])
+				output.write('set routing-instances ' + deviceName + ' interface ' + row['Interface'] + '.' + row['Unit'] +'\n')
+		if row['Routing Protocol'] != '' and  row['Routing Protocol'] != 'N/A' and row['Status'] == 'Passive':
+			output.write('set protocols ' + row['Routing Protocol'] + ' area ' + row['Area/AS/Level'] + ' interface ' + row['Interface'] + '.' + row['Unit'] + ' passive' +'\n')
+		elif row['Routing Protocol'] != '' and  row['Routing Protocol'] != 'N/A' and row['Status'] != 'Passive':
+			output.write('set protocols ' + row['Routing Protocol'] + ' area ' + row['Area/AS/Level'] + ' interface ' + row['Interface'] + '.' + row['Unit'] +'\n')
+		if row['Static Network'] != '' and row['Static Network'] != 'N/A':
+			output.write('set routing-options static route ' + row['Static Network'] + ' next-hop ' + row['Static Next-Hop'] +'\n')
+		#output.write(row['Main Router Virtual Router'],row['Instance-Type'], row['Interface'], row['Unit'], row['ip address'])
 
+# 
