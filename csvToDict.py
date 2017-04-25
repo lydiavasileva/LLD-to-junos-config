1bf032bf5d744f04ce0336b7883216a5e5cab0ed
Helweg <kenneth@helweghansen.dk>
2017-04-25 12:54:40 +0200

    Changed csv and added functions into the program

--- csvToDict.py
+++ csvToDict.py
@@ -1,28 +1,41 @@
 import csv
-output = open('setCommands','w')
-with open('IJOS-Alternative-Virtual-SRX-A1.csv', 'r') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		if (row['Interface'] != '' and row['ip address'] != '') and (row['Peer Interface'] == '' or row['Peer Interface'] == 'N/A'):
-			output.write('set interface ' + row['Interface'] + ' unit ' + row['Unit'] + ' family ' + row['Family'] + ' address ' + row['ip address']+row['Mask'] +'\n')
-		if row['Interface'] != '' and row['ip address'] != '' and row['Peer Interface'] != '' and row['Peer Interface'] != 'N/A':
-			output.write('set interface ' + row['Interface'] + ' unit ' + row['Unit'] + ' encapsulation ' + row['Encapsulation'] + ' peer-unit ' + row['Peer Unit'] + ' family ' + row['Family'] + ' address ' + row['ip address']+row['Mask'] +'\n')
-		if row['Routing-Instance'] != '' and row['Main Router Virtual Router'] != '':
-			output.write('set routing-instances ' + row['Main Router Virtual Router'] + ' instance-type ' + row['Routing-Instance'] +'\n')
+def main():
+	output = open('setCommands','w')
+	with open('IJOS-Alternative-Virtual-SRX-A1.csv', 'r') as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			setInterface(row,output)
+			setRoutingInstances(row,output)
+			setProtocols(row,output)
+			setStaticRout(row,output)
+
+def setInterface(row,output):
+	if (row['Interface'] != '' and row['ip address'] != '') and (row['Peer Interface'] == '' or row['Peer Interface'] == 'N/A'):
+				output.write('set interface ' + row['Interface'] + ' unit ' + row['Unit'] + ' family ' + row['Family'] + ' address ' + row['ip address']+row['Mask'] +'\n')
+	elif row['Interface'] != '' and row['ip address'] != '' and row['Peer Interface'] != '' and row['Peer Interface'] != 'N/A':
+				output.write('set interface ' + row['Interface'] + ' unit ' + row['Unit'] + ' encapsulation ' + row['Encapsulation'] + ' peer-unit ' + row['Peer Unit'] + ' family ' + row['Family'] + ' address ' + row['ip address']+row['Mask'] +'\n')
+	else:
+		pass
+
+def setRoutingInstances(row,output):
+	if row['Routing-Instance'] != '' and row['Main Router Virtual Router'] != '':
+		output.write('set routing-instances ' + row['Main Router Virtual Router'] + ' instance-type ' + row['Routing-Instance'] +'\n')
 		if row['Routing-Instance'] == 'virtual-router':
-			if row['Main Router Virtual Router'] != '':
-				deviceName = row['Main Router Virtual Router']
-				output.write('set routing-instances ' + deviceName + ' interface ' + row['Interface'] + '.' + row['Unit'] +'\n')
-			else:
-				output.write('set routing-instances ' + deviceName + ' interface ' + row['Interface'] + '.' + row['Unit'] +'\n')
-		if row['Routing Protocol'] != '' and  row['Routing Protocol'] != 'N/A' and row['Status'] == 'Passive':
-			output.write('set routing-instances ' + deviceName + ' protocols ' + row['Routing Protocol'] + ' area ' + row['Area/AS/Level'] + ' interface ' + row['Interface'] + '.' + row['Unit'] + ' passive' +'\n')
-		elif row['Routing Protocol'] != '' and  row['Routing Protocol'] != 'N/A' and row['Status'] != 'Passive':
-			output.write('set routing-instances ' + deviceName + ' protocols ' + row['Routing Protocol'] + ' area ' + row['Area/AS/Level'] + ' interface ' + row['Interface'] + '.' + row['Unit'] +'\n')
-		if row['Static Network'] != '' and row['Static Network'] != 'N/A':
-			output.write('set routing-options static route ' + row['Static Network'] + ' next-hop ' + row['Static Next-Hop'] +'\n')
+			output.write('set routing-instances ' + row['Main Router Virtual Router'] + ' interface ' + row['Interface'] + '.' + row['Unit'] +'\n')
+	else:
+		pass
 
+def setProtocols(row,output):
+	if row['Routing Protocol'] != '' and  row['Routing Protocol'] != 'N/A' and row['Status'] == 'Passive':
+		output.write('set routing-instances ' + row['Main Router Virtual Router'] + ' protocols ' + row['Routing Protocol'] + ' area ' + row['Area/AS/Level'] + ' interface ' + row['Interface'] + '.' + row['Unit'] + ' passive' +'\n')
+	elif row['Routing Protocol'] != '' and  row['Routing Protocol'] != 'N/A' and row['Status'] != 'Passive':
+		output.write('set routing-instances ' + row['Main Router Virtual Router'] + ' protocols ' + row['Routing Protocol'] + ' area ' + row['Area/AS/Level'] + ' interface ' + row['Interface'] + '.' + row['Unit'] +'\n')
+	else:
+		pass
 
-		#output.write(row['Main Router Virtual Router'],row['Instance-Type'], row['Interface'], row['Unit'], row['ip address'])
-
-#
+def setStaticRout(row,output):
+	if row['Static Network'] != '' and row['Static Network'] != 'N/A':
+		output.write('set routing-options static route ' + row['Static Network'] + ' next-hop ' + row['Static Next-Hop'] +'\n')
+	else:
+		pass
+main()
